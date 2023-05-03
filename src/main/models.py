f864efb335def8cd0a2e316dcebac80a279b3a89
sylverjoy <joyyamsi6@gmail.com>
2023-05-03 05:25:56 +0100

    adding new models

--- src/main/models.py
+++ src/main/models.py
@@ -8,34 +8,56 @@ from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 
+from datetime import date, datetime
+
+class School(models.Model):
+    school_id = models.CharField(max_length= 10, null=False, primary_key=True)
+    name = models.CharField(max_length= 200, null= True)
+    def __str__(self):
+        return self.name
+
+class Degree(models.Model):
+    deg_id = models.CharField(max_length=10, null= False, primary_key= True)
+    name = models.CharField(max_length= 200, null= True)
+    total_credits = models.IntegerField(null= False, default=60)   
+    def __str__(self):
+        return self.name
+
+class SemesterSession(models.Model):
+    session = models.CharField(max_length=9, null= False, blank= False)
+    semester = models.CharField(max_length= 200, null = False, choices= [('Semester 1', 'Semester 1'), ('Semester 2', 'Semester 2') ])
+    ss_id = models.CharField(max_length=9, null= False, primary_key = True)
+    semester_start = models.DateField(null= False, default= datetime.now())
+    semester_end = models.DateField(null= False, default= datetime.now())
+    ca_deadline = models.DateField(null= False, default= datetime.now())
+    active = models.BooleanField(null= True, blank=True)
+
+    def __str__(self):
+        return self.ss_id
 class Dept(models.Model):
-    dept_id = models.CharField(max_length= 3, null=False, primary_key=True)
+    school = models.ForeignKey(School, on_delete= models.CASCADE, default="")
+    dept_id = models.CharField(max_length= 10, null=False, primary_key=True)
     name = models.CharField(max_length= 200, null= True)
     def __str__(self):
         return self.dept_id
-
+    
 class Student(models.Model):
     user = models.OneToOneField(User, null = True, on_delete= models.CASCADE)
     registration_number = models.CharField(max_length= 200, null=False, primary_key=True)
+    degree_pursued = models.ForeignKey(Degree, on_delete= models.CASCADE, default="")
     dept = models.ForeignKey(Dept, on_delete= models.CASCADE)
-    # is_admin =models.BooleanField(default= True )
-    # is_student = models.BooleanField(default= False)
     name = models.CharField(max_length= 200, null= True)
     phone = models.CharField(max_length= 200, null = True)
     profile_pic = models.ImageField(null = True, blank = True, default= '0000.jpeg')
-    level = models.CharField(max_length= 200, null= True, choices= [('L1', 'HND1'), ('L2', 'HND2'), ('L3', 'BTECH')  ])
+    level = models.CharField(max_length= 200, null= True, choices= [('HND1', 'HND1'), ('HND2', 'HND2'), ('BTECH', 'BTECH')  ])
     dob = models.DateField(null = True, blank = True)
     pob = models.CharField(max_length= 200, null= True)
     
-    
-
     def __str__(self):
         return self.registration_number
 
 class AdminUser(models.Model):
     user = models.OneToOneField(User, null = True, on_delete= models.CASCADE)
-    # is_admin =models.BooleanField(default= True )
-    # is_student = models.BooleanField(default= False)
     name = models.CharField(max_length= 200, null= True)
     phone = models.CharField(max_length= 200, null = True)
     profile_pic = models.ImageField(null = True, blank = True, default="0000.jpeg")
@@ -45,8 +67,6 @@ class AdminUser(models.Model):
 
 class Teacher(models.Model):
     user = models.OneToOneField(User, null = True, on_delete= models.CASCADE)
-    # is_admin =models.BooleanField(default= True )
-    # is_student = models.BooleanField(default= False)
     name = models.CharField(max_length= 200, null= True)
     dept = models.ForeignKey(Dept, on_delete=models.CASCADE)
     teacher_id = models.CharField(max_length= 200, null=False, primary_key=True)
@@ -55,17 +75,6 @@ class Teacher(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class AssignedTeacher(models.Model):
-    student_dept = models.CharField(max_length= 3)
-    dept = models.ForeignKey(Dept,on_delete=models.CASCADE)
-    course_code = models.CharField(max_length= 200)
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = (("student_dept","course_code"))
-
 
 class AssignedTeacher2(models.Model):
     student_dept = models.CharField(max_length= 3)
@@ -76,17 +85,15 @@ class AssignedTeacher2(models.Model):
     class Meta:
         unique_together = (("teacher","course_code"))
 
-
-
 class Subject(models.Model):
     course_code = models.CharField(max_length= 200, primary_key= True)
     subject_name = models.CharField(max_length= 200)
     credit = models.FloatField(null = True)
-    semester = models.CharField(max_length= 200, null = True, choices= [('S1', 'Semester1'), ('S2', 'Semester 2') ])
+    semester = models.CharField(max_length= 200, null = True, choices= [('Semester 1', 'Semester 1'), ('Semester 2', 'Semester 2') ])
     subtype = models.CharField(max_length= 200, null=True)
     dept =models.ForeignKey(Dept, on_delete=models.CASCADE)
-    level = models.CharField(max_length= 200, null= True, choices= [('L1', 'HND1'), ('L2', 'HND2'), ('L3', 'BTECH') ])
-
+    level = models.CharField(max_length= 200, null= True, choices= [('HND1', 'HND1'), ('HND2', 'HND2'), ('BTECH', 'BTECH') ])
+    
 class RegisterTable(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     subject = models.ForeignKey(Subject,on_delete=models.CASCADE)
@@ -96,16 +103,13 @@ class RegisterTable(models.Model):
         unique_together = (("student","subject"))
 
 
-
-
 class Result(models.Model):
+    sem_ses = models.ForeignKey(SemesterSession, on_delete= models.CASCADE, default="")
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     course_code = models.CharField(max_length= 200)
     theory_marks  = models.IntegerField(null = True)
     term_test  = models.IntegerField(null = True)
-    attendence = models.IntegerField(null = True)
     total = models.FloatField(null = True)
-    dept =models.CharField(max_length= 200)
     class Meta:
         unique_together = (("student","course_code"))
     
@@ -117,7 +121,6 @@ class Rating(models.Model):
     rating  = models.IntegerField(default = '3')
     class Meta:
         unique_together = (("student","subject"))
-
 
 
 
